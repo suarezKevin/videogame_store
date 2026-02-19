cf829e58d2c608747bade5686c0351cf90342cb4
Kevin Suarez <suarezk453@gmail.com>
2026-02-19 16:21:52 -0500

    Configuration for the use of static files

--- videogame_store/videogame_store/settings.py
+++ videogame_store/videogame_store/settings.py
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+#BASE_DIR-> representa el directorio base, es decir la carpeta principal del proyecto
