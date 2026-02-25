40b8939edfc9ae121c6b497675c9055a30150443
Kevin Suarez <suarezk453@gmail.com>
2026-02-25 12:36:17 -0500

    Refactor code structure for improved readability and maintainability

--- videogame_store/catalog/models.py
+++ videogame_store/catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Videogame(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    platform = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, default='default.png')
+    
+    def __str__(self):
+        return self.name
+
+
--- videogame_store/catalog/views.py
+++ videogame_store/catalog/views.py
@@ -1,39 +1,9 @@
 from django.shortcuts import render
+from catalog.models import Videogame
 
 # Create your views here.
 def videogame_list(request):
-    videogame = [
-        {
-            'name':'Dogo Racing', 'price': 30.00, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Platform', 'price': 14.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Urban Darkness', 'price': 39.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Highspeed', 'price': 49.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Night Mode', 'price': 19.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'The Grand Thief', 'price': 59.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Sunset Vibe', 'price': 24.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Dark Whispers', 'price': 34.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Space Zero', 'price': 44.99, 'platform': 'PC,PS5,Xbox,Series X'
-        },
-        {
-            'name':'Medieval Saga', 'price': 54.99, 'platform': 'PC,PS5,Xbox,Series X'
-        }
-    ]
+    videogame = Videogame.objects.all()
     videogame_catalog = {'videogame_list': videogame}
     return render(request, 'catalog/videogame_list.html', videogame_catalog)
 
