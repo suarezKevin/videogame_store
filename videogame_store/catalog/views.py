6a21719b6ca2632b01fefa7960a7c10a3d9bfc4d
Kevin Suarez <suarezk453@gmail.com>
2026-02-25 20:23:02 -0500

    Implement pagination and navigation for videogame listing

--- videogame_store/catalog/views.py
+++ videogame_store/catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from catalog.models import Videogame
 
 # Create your views here.
 def videogame_list(request):
-    videogame = Videogame.objects.all()
-    videogame_catalog = {'videogame_list': videogame}
+    videogame = Videogame.objects.all().order_by('id')
+    
+    paginator = Paginator(videogame, 1) # Show 6 videogames for page
+    
+    # We obtain the page number from the URL
+    page_number = request.GET.get('page')
+    
+    # We obtain the objects from that page
+    page_objects = paginator.get_page(page_number)
+    
+    videogame_catalog = {'videogame_list': page_objects}
     return render(request, 'catalog/videogame_list.html', videogame_catalog)
 
