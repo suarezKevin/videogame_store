104b6be2593866bfb3822a59aa0be0b90dcc45e7
Kevin Suarez <suarezk453@gmail.com>
2026-03-06 16:27:20 -0500

    Add game detail view and template; update videogame listing with detail link

--- videogame_store/catalog/urls.py
+++ videogame_store/catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 #Enrutamiento para las páginas
 urlpatterns = [
-    path('', views.videogame_list, name='catalog')
+    path('', views.videogame_list, name='catalog'),
+    path('<int:pk>/', views.game_detail, name='game_detail')
 ]
--- videogame_store/catalog/views.py
+++ videogame_store/catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from catalog.models import Videogame
 
@@ -16,4 +16,14 @@ def videogame_list(request):
     
     videogame_catalog = {'videogame_list': page_objects}
     return render(request, 'catalog/videogame_list.html', videogame_catalog)
+
+def game_detail(request, pk):
+    # We obtein the videogame or we show a error 404
+    videogame = get_object_or_404(Videogame, pk=pk)
+    
+    # We create the context that we will pass to the template
+    context = {'game': videogame}
+    
+    # We rendered the detail template
+    return render(request, 'catalog/game_detail.html', context)
 
